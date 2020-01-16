bac0963bf446399f816fc1d392d2f6a61b6a3432
saksham-kapoor <sakshamkapoor1729@gmail.com>
2020-01-16 08:30:13 +0530

    Add Smooth Scroll

---
+++ Preus_files/myscroll.js
@@ -0,0 +1,73 @@
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar-nav");
+const navbarLinks = document.querySelectorAll(".nav-link");
+const body = document.querySelector("body");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  body.classList.toggle("nav-hovered");
+  body.classList.toggle("toggled");
+}
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navbarLinkClick);
+}
+
+function navbarLinkClick(event) {
+  smoothScroll(event);
+
+  navbarLinks.forEach(link => {
+    if (link.classList.contains("active")) {
+      link.classList.remove("active");
+    }
+  });
+  event.currentTarget.classList.add("active");
+
+  if (body.classList.contains("toggled")) {
+    navbarToggler.click();
+  }
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId =
+    event.currentTarget.getAttribute("href") === "#"
+      ? "header"
+      : event.currentTarget.getAttribute("href");
+  const targetPosition = document.querySelector(targetId).offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1500;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function linear(t, b, c, d) {
+  return (c * t) / d + b;
+}
+
+function easeInOutQuad(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
+}
